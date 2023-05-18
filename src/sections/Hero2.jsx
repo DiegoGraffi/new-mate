54f1873be84fe987d21214181599acb2a90e504d
Diego Graffigna <diegograffigna1@gmail.com>
2023-05-18 14:04:40 -0300

    mobile menu

--- src/sections/Hero2.jsx
+++ src/sections/Hero2.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { motion, AnimatePresence } from "framer-motion";
 import letter from "../../public/a-letter.png";
+import letterB from "../../public/a-letter-black.png";
 import HeroBan from "../../public/fondo.jpg";
 import logo from "../../public/logo-mate.png";
 import { MenuIcon, X, XIcon } from "lucide-react";
@@ -9,11 +11,9 @@ import { MenuIcon, X, XIcon } from "lucide-react";
 const Hero = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  function abrirMenu() {
+  function toggleMenu() {
     setMenuOpen(!menuOpen);
   }
-
-  const closeMenu = () => setMenuOpen(false);
 
   return (
     <div>
@@ -26,30 +26,21 @@ const Hero = () => {
       />
 
       <section className="h-[100vh] max-w-[1536px] mx-auto flex flex-col content-between">
-        <div className="flex flex-row justify-between p-[25px] items-center ">
-          <div>
-            <Image
-              src={letter}
-              alt="logo"
-              className="object-contain w-[30px] md:w-[50px]"
-              priority
-            />
-          </div>
+        <nav className="flex flex-row justify-between p-[25px] items-center h-[84px]">
+          <Image
+            src={letter}
+            alt="logo"
+            className="object-contain w-[30px] md:w-[50px]"
+            priority
+          />
 
-          {menuOpen ? (
-            <XIcon
-              className="md:hidden text-[#F4F4EE]"
-              size={20}
-              onClick={abrirMenu}
-            />
-          ) : (
-            <MenuIcon
-              className="md:hidden text-[#F4F4EE]"
-              size={20}
-              onClick={closeMenu}
-            />
-          )}
+          <MenuIcon
+            className="md:hidden text-[#F4F4EE]"
+            size={25}
+            onClick={toggleMenu}
+          />
 
+          {/* Navegación Desktop */}
           <ul className="hidden md:flex justify-between gap-[3px] ">
             <Link
               href="#projects"
@@ -88,7 +79,69 @@ const Hero = () => {
               </li>
             </Link>
           </ul>
-        </div>
+        </nav>
+
+        <AnimatePresence>
+          {menuOpen ? (
+            <motion.ul
+              initial={{ y: -200 }}
+              animate={{ y: 0 }}
+              transition={{ type: "spring", bounce: 0 }}
+              exit={{ y: -300 }}
+              className="bg-[#F4F4EE] md:hidden w-[100vw] flex flex-col gap-[10px] pb-[10px]  absolute"
+            >
+              <div className="flex flex-row justify-between p-[25px] items-center h-[84px]">
+                <Image
+                  src={letterB}
+                  alt="logo"
+                  className="object-contain w-[30px] md:w-[50px]"
+                  priority
+                />
+                <XIcon
+                  className="md:hidden text-[#000000] right-0"
+                  size={25}
+                  onClick={toggleMenu}
+                />
+              </div>
+              <Link
+                href="#projects"
+                scroll={false}
+                className="flex justify-center"
+              >
+                <li className="font-raleway font-[400] text-[#686868]">
+                  projects
+                </li>
+              </Link>
+              <Link
+                href="#services"
+                scroll={false}
+                className="flex justify-center"
+              >
+                <li className="font-raleway font-[400] text-[#686868]">
+                  services
+                </li>
+              </Link>
+              <Link
+                href="#about"
+                scroll={false}
+                className="flex justify-center"
+              >
+                <li className="font-raleway font-[400] text-[#686868]">
+                  about us
+                </li>
+              </Link>
+              <Link
+                href="#contact"
+                scroll={false}
+                className="flex justify-center"
+              >
+                <li className="font-raleway font-[400] text-[#686868]">
+                  contact
+                </li>
+              </Link>
+            </motion.ul>
+          ) : null}
+        </AnimatePresence>
 
         <div className="flex justify-center self-center my-auto">
           <Image
