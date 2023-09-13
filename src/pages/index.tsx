a149211f11f2340125946e0249621bd756b11c8d
emiliapujador <emiliapujador@gmail.com>
2023-09-13 13:16:11 +0100

    add source event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import Hero2 from "../sections/Hero2";
 import Projects from "../sections/Projects";
-import Image from "next/image";
 import Services from "@/sections/Services";
 import About from "@/sections/About";
 import Contact from "@/sections/Contact";
 import Head from "next/head";
 import Footer from "@/sections/Footer";
 
+import va from "@vercel/analytics";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.source) {
+      va.track(router.query.source as string);
+    }
+  }, []);
   return (
     <>
       <Head>
